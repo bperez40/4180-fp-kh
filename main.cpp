367532829db1ec03f71d371eb2e2b929e4ce7e7f
celcius-plus-273 <kelvinandreshau@gmail.com>
2023-04-12 16:45:22 +0000

    uLCD code

--- main.cpp
+++ main.cpp
@@ -1,18 +1,26 @@
 
 #include "Thread.h"
 #include "mbed.h"
+#include "mbed2/238/TARGET_LPC1768/TARGET_NXP/TARGET_LPC176X/TARGET_MBED_LPC1768/PinNames.h"
 #include "rtos.h"
 #include "SDFileSystem.h"
 #include "wave_player.h"
+#include "uLCD_4DGL.h"
 #include <cstdio>
 
 // Just checking if commits and push works
 
 SDFileSystem sd(p5, p6, p7, p8, "sd"); //SD card
+uLCD_4DGL uLCD(p13, p14, p12);  // uLCD
 
 AnalogOut DACout(p18);
 
 wave_player waver(&DACout);
+
+// uLCD setup text animation
+char* init_text_array[] = {(char *) "Initializing.", (char *) "Initializing..", (char *) "Initializing..."};
+
+
 
 void pause(const void *args) {
     Thread::wait(1000); // initial wait
